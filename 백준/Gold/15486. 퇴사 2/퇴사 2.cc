7c33a923b1b40f5ecfaf2d682199f33cd5df8286
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-01 19:21:17 +0900

    [Gold V] Title: 퇴사 2, Time: 320 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int T[1500001], P[1500001];
+int max(int a, int b) { return a > b ? a : b; }
+int main() {
+	int N, i, j, Max = 0;
+	scanf("%d", &N);
+
+	for (i = 0; i < N; i++) scanf("%d %d", &T[i], &P[i]);
+
+	for (i = N - 1; i >= 0; i--) {
+		if (i + T[i] > N)	P[i] = P[i + 1];
+		else P[i] = max(P[i + 1], P[i] + P[i + T[i]]);
+	}
+	printf("%d", P[0]);
+	return 0;
+}
