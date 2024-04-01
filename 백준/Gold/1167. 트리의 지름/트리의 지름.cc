9dddb50c952fe80a947cd2d984f0f89d7d0531be
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-01 18:49:46 +0900

    [Gold II] Title: 트리의 지름, Time: 112 ms, Memory: 7736 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <vector>
+#include <string.h>
+using namespace std;
+vector <pair<int, int>> V[100001];
+bool visited[100001];
+int node, ans;
+void DFS(int start, int value) {
+	visited[start] = true;
+	if (value > ans) ans = value, node = start;
+	for (int i = 0; i < V[start].size(); i++) {
+		int ns = V[start][i].first;
+		int nv = V[start][i].second + value;
+		if (!visited[ns]) {
+			DFS(ns, nv);
+			visited[ns] = false;
+		}
+	}
+}
+int main() {
+	int n;
+	int p, c, v;
+	int i, j;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &p);
+		while (1) {
+			scanf("%d", &c);
+			if (c == -1) break;
+			scanf("%d", &v);
+			V[p].push_back({ c,v });
+			//V[c].push_back({ p,v });
+		}
+	}
+	DFS(1, 0);
+	memset(visited, false, n + 1);
+	ans = 0;
+	DFS(node, 0);
+	printf("%d", ans);
+	return 0;
+}
