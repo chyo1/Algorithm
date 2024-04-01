cdc923f732416dd93d279f28fa29c2cf896a1619
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-01 19:20:36 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int N, K, A;
+	scanf("%d %d", &N, &K);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			scanf("%d", &A);
+		}
+	}
+	printf("비와이");
+	return 0;
+}
