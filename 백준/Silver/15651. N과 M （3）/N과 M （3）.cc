d6ec0ce2c42d5cb3b5d4ed7eb350d8d3615dda30
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-01 19:21:52 +0900

    [Silver III] Title: N과 M (3), Time: 468 ms, Memory: 1228 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+vector <int> V;
+int N, M;
+
+void Track(int cnt);
+void init(int M);
+void Print();
+
+int main() {
+	scanf("%d %d", &N, &M);
+	Track(0);
+	return 0;
+}
+
+void Track(int cnt) {
+	if (cnt == M) {
+		Print();
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		V.push_back(i);
+		Track(cnt + 1);
+		V.pop_back();
+	}
+}
+void Print() {
+	for (int i = 0; i < V.size(); i++)
+		printf("%d ", V[i]);
+	printf("\n");
+}
