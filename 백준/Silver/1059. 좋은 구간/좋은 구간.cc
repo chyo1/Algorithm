7f21f6589eacc0da3174bba52e5ab22eca5c81ad
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-01 18:42:31 +0900

    [Silver IV] Title: 좋은 구간, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1059. 좋은 구간/좋은 구간.cc
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.cc
@@ -11,23 +11,25 @@ int main() {
 
 	sort(S, S + L);
 
+	// 이 때 좋은 구간의 범위는 1~S[0]
 	if (n < S[0]) {
 		s = 1, e = S[0] - 1;
 
-		a = e - (n + 1) + 1;
-		b = (n - 1) - s + 1;
-		printf("%d", a*b + a + b);
+		a = e - n + 1;
+		b = n - s + 1;
+		printf("%d", a*b - 1);
 		return 0;
 	}
+
 	for (int i = 0; i < L - 1; i++) {
 		if (S[i] == n || S[i + 1] == n) break;
 		if (S[i] < n&&n < S[i + 1]) {
 			// 실제 범위
 			s = S[i] + 1, e = S[i + 1] - 1;
 
-			a = e - (n + 1) + 1;
-			b = (n - 1) - s + 1;
-			printf("%d", a*b + a + b);
+			a = e - n + 1;
+			b = n - s + 1;
+			printf("%d", a*b - 1);
 			return 0;
 		}
 	}
