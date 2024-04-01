07406ca7df9eed6509d9a3daa213a09cdca194f4
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-01 19:29:25 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 124 ms, Memory: 7916 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+vector <int> V[100001];
+int visited[100001];
+int N, M, R, cnt = 1;
+
+void BFS(int R) {
+	int now;
+	queue <int> Q;
+
+	// init
+	visited[R] = cnt;
+	Q.push(R);
+
+	while (!Q.empty()) {
+		now = Q.front();
+		Q.pop();
+
+		for (int i : V[now]) {
+			if (!visited[i]) {
+				Q.push(i);
+				visited[i] = ++cnt;
+			}
+		}
+	}
+	
+}
+
+int main() {
+	int a, b;
+
+	// input
+	scanf("%d %d %d", &N, &M, &R);
+	for (int i = 0; i < M; i++) {
+		scanf("%d %d", &a, &b);
+		V[a].push_back(b);
+		V[b].push_back(a);
+	}
+
+	// 오름차순 정렬
+	for (int i = 1; i <= N; i++) {
+		if (V[i].size() > 1)
+			sort(V[i].rbegin(), V[i].rend());
+	}
+
+	BFS(R);
+
+	// print
+	for (int i = 1; i <= N; i++) printf("%d\n", visited[i]);
+
+	return 0;
+}
