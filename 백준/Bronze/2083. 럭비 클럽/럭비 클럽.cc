eeeeab23e5a83ee641420f1bcfbbe627de4d9073
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-01 18:56:35 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int age, weight;
+	char name[11];
+	while (1) {
+		scanf("%s %d %d", name, &age, &weight);
+		if (name[0] == '#'&&age == 0 && weight == 0) break;
+		if (age > 17 || weight >= 80) printf("%s Senior\n", name);
+		else printf("%s Junior\n", name);
+	}
+	return 0;
+}
