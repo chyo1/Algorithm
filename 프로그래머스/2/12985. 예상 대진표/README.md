f0aa8ca8b56f082de1cfaad32a78e08f695ef53e
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-04 13:59:58 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,6 +1,6 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 13:25:13
+2024년 04월 04일 14:00:00
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -2,15 +2,19 @@
 #include <algorithm>
 using namespace std;
 
-int solution(int n, int a, int b){
-    int answer = 1;
-    if(a > b) swap(a,b);
-    while(1){
-        if(b-a==1 && a%2 && b%2==0) break;
-        answer++;
-        a = (a+1)/2;
-        b = (b+1)/2;        
+int solution(int n, int a, int b)
+{
+    int round = 1;
+
+    if (a > b) swap(a, b);
+
+    while (1) {
+       if (b - a == 1 && a % 2 && b % 2 == 0)
+           break ;
+        a = (a + 1) / 2;
+        b = (b + 1) /2;
+        round++;  
     }
 
-    return answer;
+    return round;
 }
