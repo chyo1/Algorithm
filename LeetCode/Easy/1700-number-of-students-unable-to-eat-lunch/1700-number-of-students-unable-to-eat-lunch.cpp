86b572383351d448c444dedef50b857831cc43eb
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-08 17:38:58 +0900

    Time: 2 ms (50.57%), Space: 10.8 MB (50.25%) - LeetHub

--- LeetCode/Easy/1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ LeetCode/Easy/1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,37 +1,32 @@
+#define SQUARE 0
+#define CIRCLE 1
+
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        deque <bool> foods, humans;
-
-        // 자료들을 deque에 담음
-        for(int i = 0; i < students.size(); i++) {
-            foods.push_back(sandwiches[i]);
-            humans.push_back(students[i]);
-        }
-
-        while (foods.size()) {
-            bool food = foods.front();
-            int humanNum = humans.size();
-            
-            int cnt = 0;
-
-            // 모든 학생들을 탐색했음 or 현재 학생이 먹을 샌드위치와 일치할 때까지
-            while (cnt <= humanNum && food != humans.front()) {
-                humans.push_back(humans.front());
-                humans.pop_front();
-                cnt++;
-            }
-            
-            // 모든 학생을 다 탐색함 -> 먹을 수 있는 샌드위치 없음
-            if (cnt == humanNum + 1)
-                break ;
-            
-            // 학생과 샌드위치 없앰
-            humans.pop_front();
-            foods.pop_front();
+        int cir = 0, squ = 0, size = students.size();
+        
+        for (int i = 0; i < students.size(); i++) {
+            if (students[i] == SQUARE)
+                squ++;
+            else
+                cir++;
         }
         
-        // 남아있는 학생 수 반환
-        return humans.size();
+        for (int i = 0; i < students.size(); i++) {
+            if (sandwiches[i] == CIRCLE) {
+                if (cir)
+                    cir--;
+                else
+                    break ;
+            }
+            else {
+                if (squ)
+                    squ--;
+                else
+                    break;
+            }
+        }
+        return cir + squ;
     }
 };
