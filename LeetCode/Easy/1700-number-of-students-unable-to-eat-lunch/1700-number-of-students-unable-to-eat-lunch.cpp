a735f58ac79fecd46d7632d8bd26dd03286b1dbf
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-08 17:45:33 +0900

    Time: 2 ms (50.57%), Space: 10.7 MB (95.64%) - LeetHub

--- LeetCode/Easy/1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ LeetCode/Easy/1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -6,6 +6,7 @@ public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         int cir = 0, squ = 0, size = students.size();
         
+        // 각 학생이 먹고 싶은 샌드위치의 종류 저장
         for (int i = 0; i < students.size(); i++) {
             if (students[i] == SQUARE)
                 squ++;
@@ -13,6 +14,7 @@ public:
                 cir++;
         }
         
+        // 해당 샌드위치를 먹을 학생이 없다면 반환
         for (int i = 0; i < students.size(); i++) {
             if (sandwiches[i] == CIRCLE) {
                 if (cir)
