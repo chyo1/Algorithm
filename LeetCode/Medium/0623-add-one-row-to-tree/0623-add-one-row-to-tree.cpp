419e3278d475197083fedb012a132b58df0f02ca
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-17 08:55:58 +0900

    Time: 16 ms (32.74%), Space: 24.6 MB (14.65%) - LeetHub

--- LeetCode/Medium/0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
+++ LeetCode/Medium/0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -18,41 +18,36 @@ public:
         queue<pp> q;
 
         q.push({root, 1});
-        
+
+        // depth 1일 때 예외처리
         if (depth == 1) {
             TreeNode *newRoot;
-            
-            newRoot = new TreeNode;
-            newRoot->val = val;
-            newRoot->left = root;
-            newRoot->right = NULL;
+
+            newRoot = new TreeNode(val, root, NULL);
+
             return newRoot;
-        } 
+        }
+        
         while (!q.empty()) {
             TreeNode*now = q.front().first;
             int nowDepth = q.front().second;
 
             q.pop();
 
+            // 다음 depth에 추가해야 되는 경우
             if (nowDepth + 1 == depth) {
                 TreeNode* leftChild, *rightChild;
 
-                leftChild = new TreeNode;
-                rightChild = new TreeNode;
+                // 자식 노드 추가 할당 및 val 값 입력
+                leftChild = new TreeNode(val, now->left, NULL);
+                rightChild = new TreeNode(val, NULL, now->right);
  
-                
-                leftChild->val = rightChild->val = val;
-                
-                leftChild->left = leftChild->right = rightChild->left = rightChild->right = NULL;
-                
-                if (now->left)
-                    leftChild->left = now->left;
 
-                if (now->right)
-                    rightChild->right = now->right;
-
+                // 삽입한 노드와 기존 노드 연결
                 now->left = leftChild, now->right = rightChild;
-            }      
+            }    
+            
+            // 삽입해야 하는 위치 < 주어진 depth 일 경우 큐에 추가
             else if (nowDepth < depth)
             {
                 if (now->left)
