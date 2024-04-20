ffd14cf3a3d14ef21b007ddb4a4825e5a844dab7
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-20 16:10:24 +0900

    Time: 360 ms (45.04%), Space: 170 MB (66.82%) - LeetHub

--- LeetCode/Medium/0146-lru-cache/0146-lru-cache.cpp
+++ LeetCode/Medium/0146-lru-cache/0146-lru-cache.cpp
@@ -10,22 +10,32 @@ public:
     }
     
     int get(int key) {
+        
+        // map에 없는 값이라면 -1 반환
         if (um.find(key) == um.end())
             return -1;
+        
+        // 큐에 현재 순서 삽입, usedTime에 마지막 사용시간 갱신
         q.push({cnt + 1, key});
         usedTime[key] = ++cnt;
         return um[key];
     }
     
     void put(int key, int value) {
+        
+        // um에 그 값이 없으면 LRU를 생각하지 않아도 됨
         if (um.find(key) == um.end() && um.size() == size) {
+            
+            // 큐에 저장된 시간 == 마지막 사용 시간인 값을 찾음
             while (q.front().first != usedTime[q.front().second])
                 q.pop();
             um.erase(q.front().second);
+            usedTime.erase(key);
             q.pop();
         }
         um[key] = value;
 
+        // 큐에 현재 순서 삽입, usedTime에 마지막 사용시간 갱신
         q.push({cnt + 1, key});
         usedTime[key] = ++cnt;
     }
