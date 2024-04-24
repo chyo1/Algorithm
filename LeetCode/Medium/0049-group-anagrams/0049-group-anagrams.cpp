e095c7f32d9eff6c154d50331a3a415318989c8d
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-24 15:56:13 +0900

    Time: 27 ms (64.56%), Space: 24.8 MB (61.05%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> anagrams;
-        priority_queue <pair<string, string>> pq;
+        unordered_map <string, vector<string>> um;
 
-        // 주어진 문자열을 알파벳 순서로 정렬 후 원본 문자열과 함께 우선순위 큐에 저장
+        // 주어진 문자열을 알파벳 순서로 정렬 후 원본 문자열과 함께 map에 저장
         for (int i = 0; i < strs.size(); i++) {
+            
             string origin = strs[i];
             sort(strs[i].begin(), strs[i].end());
-            pq.push({strs[i], origin});
-        }
 
-        
-        while (pq.size()) {
-            string cmp = pq.top().first;
-            vector<string> anagram;
-
-            // 정렬된 문자열이 같다 == 같은 anagram이므로 해당 값이 같을 때까지 벡터에 넣음
-            while (pq.size() && cmp == pq.top().first) {
-                anagram.push_back(pq.top().second);
-                pq.pop();
+            // 정렬 후 해당 값이 존재하지 않는다면 새로 생성
+            if (um.find(strs[i]) == um.end()) {
+                vector<string> v;
+                
+                v.push_back(origin);
+                um[strs[i]] = v;
             }
-
-            // anagram끼리 묶은 벡터 삽입
-            anagrams.push_back(anagram);
+            
+            // 존재한다면 value 값 추가
+            else
+                um[strs[i]].emplace_back(origin);
+        }
+        
+        for (auto iter = um.begin(); iter != um.end(); iter++) {
+            anagrams.push_back(iter->second);
         }
         return anagrams;
     }
