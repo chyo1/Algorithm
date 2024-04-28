538dae5bb16a64af2ad42589afbe7a2b94275e92
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-04-29 01:00:25 +0900

    Time: 17 ms (73.79%), Space: 24.6 MB (85.35%) - LeetHub

--- LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int size  = nums.size() / 2, idx = size;
-        while (size > 0) {
 
-            if (idx % 2)
-                idx++;
-            if (size % 2 && size != 1)
-                size++;
-            // printf("idx : %d nums[idx] : %d size : %d\n", idx, nums[idx], size);
+        int start = 0, end = nums.size() - 1;
 
-            // 답이 현재 idx 앞에 존재
-            if (nums[idx] == nums[idx - 1])
-                idx -= size / 2;
-            
+        while (start < end) {
+            int idx = (start + end) / 2;
+
+            // printf("idx : %d start : %d, end : %d\n", idx, start, end);
+
             // 답이 현재 idx 뒤에 존재
-            else if (nums[idx] == nums[idx + 1])
-                idx += size / 2;
-
-            // 현재 idx 가 답
+            if ((idx % 2 && nums[idx] == nums[idx - 1]) || (!(idx % 2) && nums[idx + 1] == nums[idx]))
+                start = idx + 1;
+            // 답이 현재 idx 앞에 존재 or 현재 idx
             else
-                return nums[idx];
-            size /= 2;
+                end = idx;
         }
-        return nums[0];
+        // printf("ans point : %d\n", start);
+        return nums[start];
     }
 };
