d02cc1c2141b508b910cdb57b6492ac953e0e955
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-05-08 19:04:16 +0900

    Time: 8 ms (61.94%), Space: 15.5 MB (57.12%) - LeetHub

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -6,6 +6,7 @@ public:
         for (int i = 0; i < tokens.size(); i++) {
             string token = tokens[i]; 
        
+            // 연산자라면 제일 최근에 저장된 값 2개를 꺼내 계산 후 다시 넣음
             if (token == "+" || token == "-" || token == "*" || token == "/") {
                 int num1, num2;
                 num2 = nums.top();
@@ -23,6 +24,8 @@ public:
                 else if (tokens[i] == "/")
                     nums.push(num1 / num2);
             }
+            
+            // 숫자라면 스택에 값 넣기
             else
                 nums.push(stoi(tokens[i]));
         }       
