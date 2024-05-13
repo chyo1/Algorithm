4df69427503370830f5fb9cbc8fbd264b3924dc0
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-05-13 15:54:48 +0900

    Time: 8 ms (31.30%), Space: 10.3 MB (70.79%) - LeetHub

--- LeetCode/Medium/0071-simplify-path/0071-simplify-path.cpp
+++ LeetCode/Medium/0071-simplify-path/0071-simplify-path.cpp
@@ -7,27 +7,35 @@ public:
         while(idx < len) {
             string dir = "";
             
+            // "/" 를 기준으로 디렉토리 분리
             while (idx < len && path[idx] == '/')
                 idx++;
             while (idx < len && path[idx] != '/')
                 dir += path[idx++];
             
+            // 문장 끝까지 탐색
             if (dir == "")
                 break ;
+            
+            // 이전 디렉토리, 디렉토리 하나 위로 이동
             if (dir == "..") {
                 if (directories.size())
                     directories.pop_front();
             }
+            
+            // 현재 디렉토리, 경로에 넣지 않음
             else if (dir != ".")
                 directories.push_front(dir);    
         }
         
+        // 최종 path 생성
         string ans = "";
         while (directories.size()) {
             ans += "/";
             ans += directories.back();
             directories.pop_back();
-        }           
+        } 
+        
         return ans == "" ? "/" : ans;   
     }
 };
