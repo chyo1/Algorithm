1e4eef7f120ae119f779065c0362829e62617007
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-05-23 17:27:58 +0900

    Time: 3 ms (36.96%), Space: 8.3 MB (44.84%) - LeetHub

---
+++ LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    vector<string> ans;
+    string nums[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    vector<string> letterCombinations(string digits) {
+
+        int len = digits.size();
+        if (len)
+            makeLetterCombinations(digits, "", len);
+        return ans;
+    }
+
+    void makeLetterCombinations(string &digits, string s, int len) {
+        int sLen = s.length();
+        if (sLen == len) {
+            ans.push_back(s);
+            return;
+        }
+        
+        int idx = digits[sLen] - '0';
+        for (int j = 0; j < nums[idx].size(); j++) {
+            makeLetterCombinations(digits, s + nums[idx][j], len);
+        }
+    }
+    
+};
