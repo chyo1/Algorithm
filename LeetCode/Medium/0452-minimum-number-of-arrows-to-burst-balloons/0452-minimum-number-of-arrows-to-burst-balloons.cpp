b43ab377e10eb80691e242c3f3be52084e620ee4
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-05-29 14:47:55 +0900

    Time: 266 ms (33.88%), Space: 93.2 MB (62.97%) - LeetHub

--- LeetCode/Medium/0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ LeetCode/Medium/0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
+        // 오름차순으로 정렬
         sort(points.begin(), points.end());
 
         int cnt = 0, now = 0, idx = 1;
         int size = points.size();
-
-        for (int i = 0; i < size; i++)
-            printf("%d %d \n", points[i][0], points[i][1]);
-
-        if (size == 1)
-            return 1;
-
+        
         while (now < size) {
-            int start = points[now][0], end = points[now][1];
-            while (idx < size && !(end < points[idx][0] || points[idx][1] < start)) {
-                start = max(start, points[idx][0]);
+
+            // 현재 인덱스 기준으로 end 값 지정
+            int end = points[now][1];
+
+            // idx를 늘려가며 end 범위를 좁혀감 -> 하나의 화살을 쏠 때 터뜨릴 수 있는 풍선의 개수 구함
+            while (idx < size && end >= points[idx][0]) {
                 end = min(end, points[idx][1]);
                 idx++;
             }
+
             cnt++;
             now = idx, idx += 1;
         }
