4787e4e17ee40cfa0ca14afcbdbfba307fdc65b6
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-06-03 16:34:55 +0900

    Time: 159 ms (8.37%), Space: 53.9 MB (12.45%) - LeetHub

---
+++ LeetCode/Easy/1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+
+        sort(boxTypes.begin(), boxTypes.end(), [](vector<int> a, vector<int> b){
+            return a[1] > b[1];
+        });
+
+        int maxUnits = 0;
+        for (int i = 0; i < boxTypes.size(); i++) {
+            int cnt = min(boxTypes[i][0], truckSize);
+
+            maxUnits += boxTypes[i][1] * cnt;
+            truckSize -= cnt;
+            if (!truckSize)
+                return maxUnits;
+        }
+        return maxUnits;
+    }
+};
