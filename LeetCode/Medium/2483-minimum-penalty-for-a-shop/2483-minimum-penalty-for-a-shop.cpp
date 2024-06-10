b714c022e9e74d7657f4a5601465316c4ffc61a8
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-06-10 16:40:13 +0900

    Time: 26 ms (47.19%), Space: 11.9 MB (79.05%) - LeetHub

--- LeetCode/Medium/2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ LeetCode/Medium/2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -3,17 +3,16 @@ public:
     int bestClosingTime(string customers) {
         int size = customers.size();
         int penalty = 0;
-        
-        for (int i = 0; i < size; i++) {
-            if (customers[i] == 'Y')
-                penalty++;
-        }
 
         int minPenalty = penalty, minPenaltyTime = 0;
+
+        // index를 늘려가며 close 시간을 조정
         for (int i = 0; i < size; i++) {
 
+            // open일 때 Y이면 penalty--, N이면 penalty++
             penalty = customers[i] == 'Y' ? --penalty : ++penalty;
 
+            // 최소 패널티 시점의 시간 저장
             if (minPenalty > penalty) {
                 minPenalty = penalty;
                 minPenaltyTime = i + 1;
