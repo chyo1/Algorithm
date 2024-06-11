33a110ff3aa4fea0f1bb7cdca3a9b46e5e21fdd1
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-06-11 15:32:09 +0900

    Update ［1차］ 뉴스 클러스터링.cpp

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -5,48 +5,34 @@
 using namespace std;
 
 int solution(string str1, string str2) {
-    int answer = 65536;
-    double total = 0, count = 0;
-    
-    // int sub1[700] = {0, }, sub2[700] = {0, };
-    int sub1[26][26] = {0, }, sub2[26][26] = {0, };
-    
+    double unionCnt = 0, inter = 0;
+    int substrCnt1[26][26] = {0, }, substrCnt2[26][26] = {0, };
     int lenStr1 = str1.size(), lenStr2 = str2.size();
+    
+    // str1 순회하며 서브 문자열 생성 및 해당 문자열 cnt++
     for (int i = 0; i < lenStr1 - 1; i++) {
         if(isalpha(str1[i]) && isalpha(str1[i + 1])) {
-            sub1[tolower(str1[i]) - 'a'][tolower(str1[i + 1]) - 'a']++;
-             
-            // string s;
-            // s += tolower(str1[i]);
-            // s += tolower(str1[i + 1]);
-            // cout << s << endl;
-
+            substrCnt1[tolower(str1[i]) - 'a'][tolower(str1[i + 1]) - 'a']++;
         }
     }
     
+    // str2 순회하며 서브 문자열 생성 및 해당 문자열 cnt++
     for (int i = 0; i < lenStr2 - 1; i++) {
         if(isalpha(str2[i]) && isalpha(str2[i + 1])) {
-            sub2[tolower(str2[i]) - 'a'][tolower(str2[i + 1]) - 'a']++;
-            
-            
-            // string s;
-            // s += tolower(str2[i]);
-            // s += tolower(str2[i + 1]);
-            // cout << s << endl;
+            substrCnt2[tolower(str2[i]) - 'a'][tolower(str2[i + 1]) - 'a']++;
         }
     }
     
     for (int i = 0; i < 26; i++) {
         for (int j = 0; j < 26; j++) {
-            total += (sub1[i][j] + sub2[i][j]);
-            if (sub1[i][j] && sub2[i][j]) {
-                count += min(sub1[i][j], sub2[i][j]);
-                total -= min(sub1[i][j], sub2[i][j]);
+            unionCnt += (substrCnt1[i][j] + substrCnt2[i][j]);
+            
+            // str1, str2 둘 다 값이 있다면 더 적은 쪽의 문자열 cnt가 교집합
+            if (substrCnt1[i][j] && substrCnt2[i][j]) {
+                inter += min(substrCnt1[i][j], substrCnt2[i][j]);
+                unionCnt -= min(substrCnt1[i][j], substrCnt2[i][j]); // 합집합 - 교집합
             }
         }
     }
-    // if (!total)
-    //     return answer;
-    // answer = (count * 65536 / total);
-    return !total ? answer : (count * 65536 / total);
+    return !unionCnt ? 65536 : (inter * 65536 / unionCnt);
 }
