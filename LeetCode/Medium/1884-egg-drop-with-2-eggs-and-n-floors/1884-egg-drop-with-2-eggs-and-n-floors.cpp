45db8f666a7da0005c7705447b0e54bb3ae56c89
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-06-18 17:17:50 +0900

    Time: 8 ms (53.03%), Space: 8.2 MB (33.6%) - LeetHub

---
+++ LeetCode/Medium/1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+#define MAX_VAL 10000 //
+public:
+    int twoEggDrop(int n) {
+        int dp[1001] = {1, };
+        int maxCnt = 0;
+
+        fill(dp, dp + 1001, MAX_VAL); //
+
+        dp[0] = dp[n] = 1; //
+        int step = 0, i = n;
+        while (i > 0) {
+            dp[i] = step + 1;
+            i -= ++step;
+        }
+        for (int i = 0; i <= n; i++)
+            printf("%d ", dp[i]);
+        return step;
+    }
+};
