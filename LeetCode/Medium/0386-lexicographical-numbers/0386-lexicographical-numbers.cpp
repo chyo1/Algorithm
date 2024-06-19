ba887310cb30e9b88951f6d0223fb083a45b5d61
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-06-19 15:42:44 +0900

    Time: 10 ms (66.92%), Space: 14.1 MB (34.71%) - LeetHub

--- LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -2,19 +2,28 @@ class Solution {
     vector<int> ret;
 public:
     vector<int> lexicalOrder(int n) {
-        string num;
+        int getLen = n, len = 0;
+
+        while (getLen > 0) {
+            getLen /= 10;
+            len++;
+        }
         for (int i = 1; i < 10; i++) {
-            num = to_string(i);
-            getLexicographicalNum(num, n);
+            int num = i;
+            getLexicographicalNum(num, n, 1);
         }
         return ret;
     }
-    void getLexicographicalNum(string num, int n){
-        if (stoi(num) > n)
-            return ;
-        ret.push_back(stoi(num));
-        for (int i = 0; i < 10; i++) {
-            getLexicographicalNum(num + to_string(i), n);
+    bool getLexicographicalNum(int num, int n, int len){
+        
+        for (int i = 0; i < min(len, 10); i++) {
+            int number = num + i;
+            if (number > n)
+                return true;
+            ret.push_back(number);
+            // printf("num: %d\n", number);
+            getLexicographicalNum(number * 10, n, len * 10);
         }
+        return false;
     }
 };
