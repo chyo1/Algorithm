c0f51ceb8ea615b95101319aa0a09c75c0a1f374
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-06-26 00:56:35 +0900

    Time: 8 ms (81.68%), Space: 20.6 MB (44.95%) - LeetHub

--- LeetCode/Medium/0057-insert-interval/0057-insert-interval.cpp
+++ LeetCode/Medium/0057-insert-interval/0057-insert-interval.cpp
@@ -1,57 +1,24 @@
 class Solution {
 public:
-    #define MAX_VAL 100001
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int arr[MAX_VAL] = {0, };
-        bool checkSameRange[MAX_VAL] = {false, };
-        vector<vector<int>> answer;
+        vector<vector<int>> ans;
+        int size = intervals.size();
 
-        for (int i = 0; i < intervals.size(); i++) {
-            vector<int> now = intervals[i];
+        int i = 0;
+        while (i < size && intervals[i][1] < newInterval[0])
+            ans.push_back(intervals[i++]);
 
-            // 시작 지점 증가, 종료 지점 감소
-            if (now[0] != now[1])
-                arr[now[0]]++, arr[now[1]]--;
-            else
-                checkSameRange[now[0]] = true;
+        int start = newInterval[0], end = newInterval[1];
+        while (i < size && intervals[i][0] <= newInterval[1]) {
+            start = min(start, intervals[i][0]);
+            end = max(end, intervals[i][1]);
+            i++;
         }
-
-        // 새로 입력된 간격 추가
-        if (newInterval[0] != newInterval[1])
-            arr[newInterval[0]]++, arr[newInterval[1]]--;
-        else
-            checkSameRange[newInterval[0]] = true;
-
-        int start, end, nowVal;
-        for (int i = 0; i < MAX_VAL; i++) {
-
-            if (checkSameRange[i] && !arr[i]) {
-                // 해당 구간 추가
-                vector<int> element = {i, i};
-                answer.push_back(element);
-            }
-
-            // 시작지점
-            if (arr[i] > 0) {
-                start = i;
-                nowVal = arr[i];
-
-                // 해당 구간이 끝날 때까지
-                while (nowVal != 0) {
-                    // if (checkSameRange[i]) {
-                    //     vector<int> element = {start, i};
-                    //     answer.push_back(element);
-                    //     start = i;
-                    // }
-                    nowVal += arr[++i];
-                }
-
-                // 해당 구간 추가
-                vector<int> element = {start, i};
-                answer.push_back(element);
-            }
-
-        }
-        return answer;
+        ans.push_back({start, end});
+        
+        while (i < size)
+            ans.push_back(intervals[i++]);
+        
+        return ans;
     }
 };
