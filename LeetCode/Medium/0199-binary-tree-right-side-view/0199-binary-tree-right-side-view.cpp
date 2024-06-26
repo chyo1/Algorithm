8a414b0e7220596f1a535172e05de4e458dbc503
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-06-26 12:36:43 +0900

    Time: 0 ms (100%), Space: 13.9 MB (74.4%) - LeetHub

---
+++ LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    int rightSideVisited[101] = {false, };
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root)
+            return ans;
+
+        getRightSideView(root, 0, true);
+        return ans;
+    }
+    void getRightSideView(TreeNode* now, int lev, bool isRight) {
+        if (isRight && !rightSideVisited[lev]) {
+            ans.push_back(now->val);
+            rightSideVisited[lev] = true;
+        }
+        else if (!rightSideVisited[lev]) {
+            ans.push_back(now->val);
+            rightSideVisited[lev] = true;
+        }
+
+        if (now->right)
+            getRightSideView(now->right, lev + 1, true);
+        if (now->left)
+            getRightSideView(now->left, lev + 1, false);
+
+    }
+};
