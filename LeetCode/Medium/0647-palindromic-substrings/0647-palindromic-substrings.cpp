80711975aab500695082d8f109de50457a998537
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-06-30 15:24:31 +0900

    Time: 11 ms (54.66%), Space: 8.4 MB (66.44%) - LeetHub

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -5,20 +5,20 @@ public:
         bool dp[1001][1001] = {false, };
 
         for (int i = 0; i < size; i++) {
-            dp[i][1] = true;
+            dp[i][i] = true;
             cnt++;
         }
         for (int i = 0; i < size - 1; i++) {
             if (s[i] == s[i + 1]) {
-                dp[i][2] = true;
+                dp[i][i + 1] = true;
                 cnt++;
             }
         }
 
         for (int len = 3; len <= size; len++) {
             for (int idx = 0; idx <= size - len; idx++) {
-                if (dp[idx + len - 2][len - 2] && s[idx] == s[idx + len - 1]) {
-                    dp[idx][len - 1] = true;
+                if (dp[idx + 1][idx + len - 2] && s[idx] == s[idx + len - 1]) {
+                    dp[idx][idx + len - 1] = true;
                     cnt++;
                 }
             }
