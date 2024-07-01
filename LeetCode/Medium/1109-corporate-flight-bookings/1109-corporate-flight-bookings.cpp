2a69db9fcc831ace1ba19ce85260a9e2d4b40c18
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-01 17:10:56 +0900

    Time: 140 ms (85.63%), Space: 72.6 MB (38.78%) - LeetHub

--- LeetCode/Medium/1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ LeetCode/Medium/1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> res(n, 0);
-        for (int i = 0; i < bookings.size(); i++) {
-            int s = bookings[i][0], e = bookings[i][1], seat = bookings[i][2];
-            for (int j = s - 1; j < e; j++) {
-                res[j] += seat;
-            }
+        int total[20002] = {0, };
+        vector<int> res;
+        int size = bookings.size();
+        for (int i = 0; i < size; i++) {
+            int first = bookings[i][0], last = bookings[i][1], seats = bookings[i][2];
+            total[first] += seats;
+            total[last + 1] -= seats;
+        }
+
+        int flight = 0;
+        for (int i = 1; i <= n; i++) {
+            flight += total[i];
+            res.push_back(flight);
         }
         return res;
     }
