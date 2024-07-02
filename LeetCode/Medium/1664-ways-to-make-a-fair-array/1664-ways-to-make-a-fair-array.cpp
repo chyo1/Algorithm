e8c47d4ab4dba5d9773b06e20740fcff7d560f56
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-02 21:43:39 +0900

    Time: 348 ms (5.28%), Space: 97.3 MB (59.32%) - LeetHub

---
+++ LeetCode/Medium/1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int waysToMakeFair(vector<int>& nums) {
+        int sum[100002], cnt = 0;
+        int oddSum = 0, evenSum = 0, size = nums.size();
+
+        if (size == 1)
+            return 1;
+        sum[0] = evenSum = nums[0], sum[1] = oddSum = nums[1];
+        for (int i = 2; i < size; i++) {
+            sum[i] = sum[i - 2] + nums[i];
+            if (i % 2 == 0)
+                evenSum += nums[i];
+            else
+                oddSum += nums[i];
+        }
+        for (int i = 0; i < size; i++)
+            printf("%d ", sum[i]);
+        printf("\nevenSum: %d, oddSum: %d\n", evenSum, oddSum);
+
+        int odd, even;
+        for (int i = 0; i < size; i++) {
+
+            if (i == 0) {
+                if (evenSum - nums[i] == oddSum)
+                    cnt++;
+            }
+
+            // 홀수 합
+            else if (i % 2) {
+                odd = evenSum - sum[i - 1];
+                if (i != 1)
+                    odd += sum[i - 2];
+
+                even = sum[i - 1] + (oddSum - sum[i]);
+            }
+
+            // 짝수 합
+            else {
+                even = oddSum - sum[i - 1];
+                if (i != 0)
+                    even += sum[i - 2];
+
+                odd = sum[i - 1] + (evenSum - sum[i]);
+            }
+
+            if (odd == even) {
+                cnt++;
+                // printf("i: %d\n", i);
+            }
+        }
+        return cnt;
+    }
+};
+/*
+2 1 6 4 5 7 8
+
+2 1 8 5
+
+0 1 2 3
+*/
