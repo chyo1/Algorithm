336d5a1da9516211f2d99e81bd2204578f6a4490
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-03 15:21:18 +0900

    Time: 49 ms (46.77%), Space: 17.2 MB (97.1%) - LeetHub

--- LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -2,28 +2,20 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
+        int row = 0, col = n - 1;
         
+        // target > matrix의 최댓값
         if (matrix[m - 1][n - 1] < target)
             return false;
 
-        for (int i = 0; i < m; i++) {
-            if (target < matrix[i][0])
-                return false;
-            if (matrix[i][n - 1] < target)
-                continue;
-
-            int start = 0, end = n;
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (matrix[i][mid] == target)
-                    return true;
-                else if (matrix[i][mid] < target)
-                    start = mid + 1;
-                else
-                    end = mid - 1;
-            }
+        while (row < m && 0 <= col) {
+            if (matrix[row][col] == target)
+                return true;
+            if (matrix[row][col] < target)
+                row++;
+            else
+                col--;
         }
-
         return false;
     }
 };
