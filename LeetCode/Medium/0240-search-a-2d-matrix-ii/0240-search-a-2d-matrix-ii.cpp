abc9e182ddb4eab9f09ded57dae2c4887d7444a0
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-03 14:46:42 +0900

    Time: 2454 ms (5.02%), Space: 17.5 MB (11.44%) - LeetHub

--- LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
-
-        int row = 0, col = 0;
         
-        int start = 0, end = m;
-        while (start < end) {
-            int mid = (start + end) / 2;
+        if (matrix[m - 1][n - 1] < target)
+            return false;
 
-            if (matrix[row][mid] == target)
-                return true;
-            else if (matrix[row][mid] < target)
-                start = mid;
-            else
-                end = mid;
+        for (int i = 0; i < m; i++) {
+            if (target < matrix[i][0])
+                return false;
+            if (matrix[i][n - 1] < target)
+                continue;
 
+            int start = 0, end = n;
+            while (start <= end) {
+                int mid = (start + end) / 2;
+
+                printf("s %d e %d m %d\n", start, end, mid);
+                if (matrix[i][mid] == target)
+                    return true;
+                else if (matrix[i][mid] < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
         }
-        col = mid;
-
-         while (start < end) {
-            int mid = (start + end) / 2;
-
-            if (matrix[mid][col] < target)
-                start = mid;
-            else
-                end = mid;
-
-        }
-        row = mid;
 
         return false;
     }
