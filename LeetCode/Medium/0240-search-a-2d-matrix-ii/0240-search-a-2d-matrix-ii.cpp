86af221ae563be860aeccc2c6838082d7f58876b
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-03 14:46:34 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,12 +3,33 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == target)
-                    return true;
-            }
+        int row = 0, col = 0;
+        
+        int start = 0, end = m;
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (matrix[row][mid] == target)
+                return true;
+            else if (matrix[row][mid] < target)
+                start = mid;
+            else
+                end = mid;
+
         }
+        col = mid;
+
+         while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (matrix[mid][col] < target)
+                start = mid;
+            else
+                end = mid;
+
+        }
+        row = mid;
+
         return false;
     }
 };
