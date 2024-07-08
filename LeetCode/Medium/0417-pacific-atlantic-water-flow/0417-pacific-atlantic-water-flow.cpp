8341d0f872ea51e44e3b4128420056e583165fc0
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-08 18:34:59 +0900

    Time: 23 ms (92.59%), Space: 19.8 MB (98.85%) - LeetHub

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,43 +1,43 @@
 class Solution {
 
 private:
-        pair<bool,bool> accessable[200][200] = {};
-        bool visited[200][200] = {false, };
-        int mv[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        int row, col;
+    pair<bool,bool> accessable[200][200] = {};
+    bool visitedPacific[200][200] = {false, }, visitedAtlantic[200][200] = {false, };
+    int mv[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int row, col;
 
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         vector<vector<int>> ans;
         row = heights.size(), col = heights[0].size();
 
-        // pacific 초기화
+        // // pacific 초기화
+        // for (int i = 0; i < row; i++) {
+        //     visitedPacific[i][0] = true;
+        //     visitedAtlantic[i][col - 1] = true;
+        // }
+
+        // // atlantic 초기화
+        // for (int j = 0; j < col; j++) {
+        //     visitedPacific[0][j] = true;
+        //     visitedAtlantic[row - 1][j] = true;
+        // }
+
         for (int i = 0; i < row; i++) {
-            accessable[i][0].first = true;
-            accessable[i][col - 1].second = true;
+            checkPacific(heights, i, 0);
+            checkAtlantic(heights, i, col - 1);
         }
 
-        // atlantic 초기화
         for (int j = 0; j < col; j++) {
-            accessable[0][j].first = true;
-            accessable[row - 1][j].second = true;
+            checkPacific(heights, 0, j);
+            checkAtlantic(heights, row - 1, j);
         }
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
 
-                // 해당 칸에서 둘 다 접근 가능한 지 확인
-                fill(visited[0], visited[row], false);
-                if (checkPacific(heights, i, j))
-                    accessable[i][j].first = true;
-
-                fill(visited[0], visited[row], false);
-                if (checkAtlantic(heights, i, j))
-                    accessable[i][j].second = true;
-                
-
                 // 둘 다 접근 가능하면 답에 추가
-                if (accessable[i][j].first && accessable[i][j].second) {
+                if (visitedPacific[i][j] && visitedAtlantic[i][j]) {
                     vector<int> co = {i, j};
                     ans.push_back(co);
                 }
@@ -46,39 +46,28 @@ public:
         return ans;
     }
 
-    bool checkPacific(vector<vector<int>>& heights, int r, int c) {
-        if (accessable[r][c].first)
-            return true;
-        visited[r][c] = true;
+    void checkPacific(vector<vector<int>>& heights, int r, int c) {
+        visitedPacific[r][c] = true;
         for (int i = 0; i < 4; i++) {
             int nr = r + mv[i][0], nc = c + mv[i][1];
-            if (nr < 0 || nr >= row || nc < 0 || nc >= col || visited[nr][nc]) continue;
-
-            if (heights[r][c] >= heights[nr][nc]) {
-                if (checkPacific(heights, nr, nc)) {
-                    accessable[nr][nc].first = true;
-                    return true;
-                }
-            }
+            if (nr < 0 || nr >= row || nc < 0 || nc >= col || visitedPacific[nr][nc]) continue;
+            
+            if (heights[r][c] <= heights[nr][nc])
+                checkPacific(heights, nr, nc);
         }
-        return false;
+        return;
     }
 
-    bool checkAtlantic(vector<vector<int>>& heights, int r, int c) {
-        if (accessable[r][c].second)
-            return true;
-        visited[r][c] = true;
+    void checkAtlantic(vector<vector<int>>& heights, int r, int c) {
+        visitedAtlantic[r][c] = true;
         for (int i = 0; i < 4; i++) {
             int nr = r + mv[i][0], nc = c + mv[i][1];
-            if (nr < 0 || nr >= row || nc < 0 || nc >= col || visited[nr][nc]) continue;
+            if (nr < 0 || nr >= row || nc < 0 || nc >= col || visitedAtlantic[nr][nc]) continue;
 
-            if (heights[r][c] >= heights[nr][nc]) {
-                if (checkAtlantic(heights, nr, nc)) {
-                    accessable[nr][nc].second = true;
-                    return true;
-                }
-            }
+            if (heights[r][c] <= heights[nr][nc])
+                checkAtlantic(heights, nr, nc);
         }
-        return false;
+        
+        return;
     }
 };
