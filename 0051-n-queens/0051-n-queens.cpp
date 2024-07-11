09cb86778443a201861f51c4fc0d5e746d382703
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-11 12:45:52 +0900

    Time: 7 ms (41.76%), Space: 10.6 MB (42.61%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,13 +2,16 @@ class Solution {
 private:
     int line[10] = {0, };
     bool visited[10] = {false, };
+    
 public:
     vector<vector<string>> ans;
+
     vector<vector<string>> solveNQueens(int n) {
         getNQueens(1, n);
         return ans;
     }
 
+    // 각 케이스를 답 벡터에 추가
     void makeAns(int n) {
         vector<string> caseOne;
         for (int i = 1; i <= n; i++) {
@@ -21,20 +24,26 @@ public:
         ans.push_back(caseOne);
     }
 
+    // 대각선 상에 값이 존재하는지 확인
     bool isPossibleLine(int val, int end) {
         for (int i = 1; i < end; i++) {
+            // row 차이 == col 차이 : 같은 대각선 상에 존재
             if (abs(val - line[i]) == abs(end - i))
                 return false;
         }
         return true;
     }
+
     void getNQueens(int col, int n) {
+
+        // 표를 다 채웠다면 답에 추가
         if (col == n + 1) {
             makeAns(n);
             return ;
         }
         for (int i = 1; i <= n; i++) {
             if (visited[i]) continue;
+
             if (col == 1 || isPossibleLine(i, col)) {
                 visited[i] = true;
                 line[col] = i;
@@ -44,5 +53,3 @@ public:
         }
     }
 };
-
-// 2 4 1 5 3
