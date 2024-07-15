c95a0b8912b435a89f4df1db152998c322272374
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-15 15:07:32 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        TreeNode* root = NULL;
+        unordered_set<int> childs;
+        unordered_map<int, TreeNode*> nodes;
+
+        for (int i = 0; i < descriptions.size(); i++) {
+            int parentVal = descriptions[i][0], childVal = descriptions[i][1], isLeft = descriptions[i][2];
+
+            TreeNode* newChild = findNode(nodes, childVal);
+            TreeNode* newParent = findNode(nodes, parentVal);
+
+            childs.insert(childVal);
+
+            if (!newChild) {
+                newChild = new TreeNode(childVal);
+                nodes.insert({childVal, newChild});
+            }
+            if (!newParent) {
+                newParent = new TreeNode(parentVal);
+                nodes.insert({parentVal, newParent});
+                if (childs.find(parentVal) == childs.end())
+                    root = newParent;
+            }
+            if (isLeft)
+                newParent->left = newChild;
+            else
+                newParent->right = newChild;
+        }
+        return root;
+    }
+
+    TreeNode* findNode(const unordered_map<int, TreeNode*>& nodes, int val) {        
+        auto iter = nodes.find(val);
+
+        if (iter == nodes.end())
+            return NULL;
+        
+        return iter->second;
+    }
+};
