8e2f920b26847d8c3074715d8a7159e8a95d3f3e
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-17 20:51:00 +0900

    Time: 11 ms (36.83%), Space: 18.1 MB (12.49%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -5,32 +5,14 @@ public:
         int start = 0, end = numbers.size() - 1;
         while (1) {
             int sum = numbers[start] + numbers[end];
-            if (sum == target) {
-                // if (start != end)
-                //     break;
-                // if (start > 0 && numbers[start - 1] + numbers[end] == target) {
-                //     start--;
-                //     printf("start\n");
-                // }
-                // else if (end < numbers.size() - 1 && numbers[start] + numbers[end + 1] == target) {
-                //     end++;
-                //     printf("end\n");
-                // }
-                // else {
-                //     start--; end++;
-                //     printf("other\n");
-                // }
-                // if (numbers[start] + numbers[end] == sum)
-                    break;
-            }
+            if (sum == target)
+                break;
             else if (sum < target)
                 start++;
             else
                 end--;
         }
-        // printf("%d %d\n", numbers[start], numbers[end]);
         ans[0] = start + 1, ans[1] = end + 1;
-        // sort(ans.begin(), ans.end());
         return ans;
     }
 };
