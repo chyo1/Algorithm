90b47def2bf95fbb880210609e7b9a9e63995afa
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-23 15:02:12 +0900

    Time: 97 ms (88.17%), Space: 81 MB (18.64%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.cpp
+++ 2105-watering-plants-ii/2105-watering-plants-ii.cpp
@@ -4,6 +4,7 @@ public:
         int size = plants.size(), half = size / 2;
         int cntA = 0, cntB = 0, capA = capacityA, capB = capacityB, idxA = 0, idxB = size - 1;
 
+        // A 인덱스 증가, B 인덱스 감소시켜가면서 탐색
         while (idxB - idxA > 0) {
             if (capA >= plants[idxA])
                 capA -= plants[idxA];
@@ -21,6 +22,7 @@ public:
             idxA++; idxB--;
         }
 
+        // 전체 갯수가 홀수일 경우
         if (idxB == idxA) {
             if (capA < capB) {
                 if (capB < plants[idxA])
