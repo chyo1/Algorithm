f3c0ddf6f568798c430efbae21ee344dbc1ae6e6
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-23 15:04:02 +0900

    Time: 118 ms (17.2%), Space: 80.8 MB (29.75%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.cpp
+++ 2105-watering-plants-ii/2105-watering-plants-ii.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int minimumRefill(vector<int>& plants, int capacityA, int capacityB) {
         int size = plants.size(), half = size / 2;
-        int cntA = 0, cntB = 0, capA = capacityA, capB = capacityB, idxA = 0, idxB = size - 1;
+        int cnt = 0, capA = capacityA, capB = capacityB, idxA = 0, idxB = size - 1;
 
         // A 인덱스 증가, B 인덱스 감소시켜가면서 탐색
         while (idxB - idxA > 0) {
             if (capA >= plants[idxA])
                 capA -= plants[idxA];
             else {
-                cntA++;
+                cnt++;
                 capA = capacityA - plants[idxA];
             }
 
             if (capB >= plants[idxB])
                 capB -= plants[idxB];
             else {
-                cntB++;
+                cnt++;
                 capB = capacityB - plants[idxB];
             }
             idxA++; idxB--;
@@ -26,14 +26,14 @@ public:
         if (idxB == idxA) {
             if (capA < capB) {
                 if (capB < plants[idxA])
-                    cntB++;
+                    cnt++;
             }
             else {
                 if (capA < plants[idxA])
-                    cntA++;
+                    cnt++;
             }
         }
 
-        return cntA + cntB;
+        return cnt;
     }
 };
