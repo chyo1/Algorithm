a4fa433854c62f69a4426438a8286156f850df3f
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-07-28 22:23:53 +0900

    Time: 26 ms (90.34%), Space: 36 MB (69.23%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -6,22 +6,19 @@ public:
 
         int wordLen = searchWord.size(), listCnt = products.size();
         for (int i = 0; i < wordLen; i++) {
-
-            // 해당 단어가 들어있는 첫 번째 위치 찾기
-            int start = lower_bound(products.begin(), products.end(), searchWord.substr(0, i + 1)) - products.begin();
+            string findWord = searchWord.substr(0, i + 1);
             
-            // 해당 위치 + 2개, 주어진 배열의 마지막 원소 중 작은 인덱스 찾기
-            int end = min(listCnt - 1, start + 2);
-
-            // 겹치는 단어가 있을 때까지 end--
-            while (start <= end && products[end].substr(0, i + 1) != searchWord.substr(0, i + 1)){
-                end--;
-            };
-
-            // 답 만들기
+            // 해당 단어가 들어있는 첫 번째 위치 찾기
+            int start = lower_bound(products.begin(), products.end(), findWord) - products.begin();
+            
+            // 첫 위치부터 3개 탐색하며 반환할 배열값 찾기
             vector<string> sub;
-            for (int j = start; j <= end; j++)
-                sub.push_back(products[j]);
+            for (int j = 0; j < 3; j++) {
+                if (start + j >= listCnt) break;
+                if (products[start + j].substr(0, i + 1) != findWord)
+                    break;
+                sub.push_back(products[start + j]);
+            }
             ans.push_back(sub);
         }
         return ans;
