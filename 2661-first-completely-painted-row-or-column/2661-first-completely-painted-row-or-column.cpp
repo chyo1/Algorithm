ad31ac10483926cc95f2023beaa2ac2fb44436a3
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-08-05 16:10:50 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        pair<int, int> loc[m * n + 1];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                loc[mat[i][j]] = {i, j};
+            }
+        }
+
+        int row[100001] = {0, }, col[100001] = {0, };
+        int size = arr.size();
+        for (int i = 0; i < size; i++) {
+            pair<int, int> cor = loc[arr[i]];
+
+            row[cor.first]++, col[cor.second]++;
+
+            if (row[cor.first] == m || col[cor.second] == n)
+                return i;
+        }
+        return size - 1;
+    }
+};
