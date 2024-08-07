e2bdd258aa272eaceaaae761bbc4e0ec1732bba8
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-08-07 18:13:45 +0900

    Time: 168 ms (48.36%), Space: 99.2 MB (21.23%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -13,11 +13,14 @@ public:
         for (auto& [n2, n1] : nums) {
             sum += n1;
             pq.emplace(n1);
-            
-            if (pq.size() == k) {
-                ans = max(ans, sum * n2);
+
+            if (pq.size() > k) {
                 sum -= pq.top();
                 pq.pop();
+            }
+
+            if (pq.size() == k) {
+                ans = max(ans, sum * n2);
             }
         }
         return ans;
