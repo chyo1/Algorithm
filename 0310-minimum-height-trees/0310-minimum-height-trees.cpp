75024269cd7d62a8fce58a10db687a6e33f086cd
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-08-27 15:54:09 +0900

    Time: 609 ms (5.03%), Space: 58.6 MB (56.26%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n <= 1)
+            return {0};
+        vector<int> graph[n], ans;
+        for (auto edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (graph[i].size() == 1)
+                ans.push_back(i);
+        }
+
+        while (n > 2) {
+            n -= ans.size();
+
+            vector<int> nextNodes;
+            for (auto node : ans) {
+                int pairNode = graph[node][0];
+                graph[pairNode].erase(remove(graph[pairNode].begin(), graph[pairNode].end(), node),  graph[pairNode].end());
+                if (graph[pairNode].size() == 1)
+                    nextNodes.push_back(pairNode);
+                
+                cout << graph[pairNode].size() << endl;
+            }
+            ans.clear();
+            ans = nextNodes;
+        }
+        return ans;
+    }
+};
