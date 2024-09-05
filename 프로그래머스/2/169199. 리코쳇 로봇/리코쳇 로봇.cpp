241311b630512d47671cc9ff950d8cc9842e43f9
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-09-05 23:01:21 +0900

    Update 리코쳇 로봇.cpp

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.cpp
@@ -1,34 +1,14 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
+
 using namespace std;
 
 bool visited[100][100];
-int r, c, sx, sy, ex, ey, ans = 1000000000;
-int mv[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-bool dfs(vector<string>& board, int x, int y, int cnt) {
-    for (int i = 0; i < 4; i++) {
-        int nx = x, ny = y;
-        while (0 <= nx && nx < r && 0 <= ny && ny < c && board[nx][ny] != 'D') {
-            nx += mv[i][0], ny += mv[i][1];
-        }
-        nx -= mv[i][0], ny -= mv[i][1];
-        if (board[nx][ny] == 'G') {
-            ans = min(cnt, ans);
-            return false;
-        }
-        if (visited[nx][ny]) continue;
-        visited[nx][ny] = true;
-        if (dfs(board, nx, ny, cnt + 1))
-            return true;
-    }
-    return false;
-}
 
 int solution(vector<string> board) {
-    r = board.size(), c = board[0].size();
+    int r = board.size(), c = board[0].size();
+    int sx, sy;
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
             if (board[i][j] == 'R') {
@@ -37,18 +17,21 @@ int solution(vector<string> board) {
             }
         }
     }
-    visited[sx][sy] = true;
+
     queue<pair<pair<int, int>, int>> q;
+    int mv[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     
+    visited[sx][sy] = true;
     q.push({{sx, sy}, 0});
+    
     while (!q.empty()) {
         int x = q.front().first.first, y = q.front().first.second, cnt = q.front().second;
         q.pop();
+        
         for (int i = 0; i < 4; i++) {
             int nx = x, ny = y;
-            while (0 <= nx && nx < r && 0 <= ny && ny < c && board[nx][ny] != 'D') {
+            while (0 <= nx && nx < r && 0 <= ny && ny < c && board[nx][ny] != 'D')
                 nx += mv[i][0], ny += mv[i][1];
-            }
             nx -= mv[i][0], ny -= mv[i][1];
             
             if (board[nx][ny] == 'G')
@@ -60,6 +43,4 @@ int solution(vector<string> board) {
         }
     }
     return -1;
-    // dfs(board, sx, sy, 0);
-    return ans == 1000000000 ? -1 : ans;
 }
