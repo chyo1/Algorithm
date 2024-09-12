931d186a4ada0111ec6dfc5d5dbd24839fe445b4
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-09-12 23:28:31 +0900

    Time: 0 ms (100%), Space: 7.6 MB (47%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,64 +1,28 @@
 class Solution {
 public:
-    typedef pair<int, int> pp;
-    bool visited[2001][2001];
+    bool visited[2001];
     bool canMeasureWater(int x, int y, int target) {
         return bfs(x, y, target);
     }
 
     bool bfs(int x, int y, int target) {
-        queue<pp> q;
+        queue<int> q;
+        int waters[4] = {x, y, -x, -y}, totalWater = x + y;
 
-        q.push({0, y});
-        q.push({x, 0});
-        visited[0][y] = visited[x][0] = true;
+        q.push(0);
+        visited[0] = true;
 
         while (!q.empty()) {
-            pp now = q.front();
+            int now = q.front();
             q.pop();
 
-            // cout << now.first << " " << now.second << endl;
-
-            if (now.first == target || now.second == target || now.first + now.second == target)
+            if (now == target)
                 return true;
-
-            // 채우기
-            if (!visited[now.first][y]) {
-                q.push({now.first, y});
-                visited[now.first][y] = true;
-            }
-            if (!visited[x][now.second]) {
-                q.push({x, now.second});
-                visited[x][now.second] = true;
-            }
-
-            // 버리기
-            if (now.first && !visited[now.first][0]) {
-                q.push({now.first, 0});
-                visited[now.first][0] = true;
-            }
-            if (now.second && !visited[0][now.second]) {
-                q.push({0, now.second});
-                visited[0][now.second] = true;
-            }
-
-            // 옆으로 넘기기
-            if (now.first + now.second > x) {
-                if (!visited[x][now.second - (x - now.first)])
-                    q.push({x, now.second - (x - now.first)});
-            }
-            else {
-                if (!visited[now.first + now.second][0])
-                q.push({now.first + now.second, 0});
-            }
-
-            if (now.first + now.second > y) {
-                if (!visited[now.first - (y - now.second)][y])
-                    q.push({now.first - (y - now.second), y});
-            }
-            else {
-                if (!visited[0][now.first + now.second])
-                q.push({0, now.first + now.second});
+            for (int w : waters) {
+                if (0 <= now + w && now + w <= totalWater && !visited[now + w]) {
+                    q.push(now + w);
+                    visited[now + w] = true;
+                }
             }
 
         }
