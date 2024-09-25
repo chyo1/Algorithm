a1f3bea884cd4898b55b9cc50f1dddc3c8b40e7f
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-09-25 11:43:31 +0900

    Time: 200 ms (61.92%), Space: 115.2 MB (92.55%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> man[100000];
+
+    int dfs(int nowID, vector<int>& informTime) {
+        int maxInformTime = 0;
+        for (int subordinate : man[nowID]) {
+            maxInformTime = max(dfs(subordinate, informTime), maxInformTime);
+        }
+        return maxInformTime + informTime[nowID];
+    }
+
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        for (int i = 0; i < n; i++) {
+            int superior = manager[i];
+            if (superior == -1) continue;
+            man[manager[i]].push_back(i);
+        }
+        
+        return dfs(headID, informTime);
+    }
+};
