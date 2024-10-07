2e8ef9ddc45f184b87c4ad6c32329d47a1355fe1
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-10-07 14:07:50 +0900

    Time: 65 ms (48.22%), Space: 9.4 MB (72.46%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        // dp[len][idx]
+        bool dp[1001][1000] = {false, }; 
+        
+        int size = s.size(), maxLen = 1, maxLoc = 0;
+
+        for (int len = 1; len <= size; len++) {
+            for (int idx = 0; idx <= size - len; idx++) {
+                if (len == 1) 
+                    dp[len][idx] = true;
+
+                else if (len == 2) {
+                    if (s[idx] == s[idx + 1]) {
+                        dp[len][idx] = true;
+
+                        if (maxLen < len) {
+                            maxLen = len;
+                            maxLoc = idx;
+                        }
+                    }
+                }
+
+                else {
+                    if (dp[len - 2][idx + 1] && s[idx] == s[idx + len - 1]) {
+                        dp[len][idx] = true;
+
+                        if (maxLen < len) {
+                            maxLen = len;
+                            maxLoc = idx;
+                        }
+                    }
+                }
+            }
+        }
+        return s.substr(maxLoc, maxLen);
+    }
+};
+
+// baab
