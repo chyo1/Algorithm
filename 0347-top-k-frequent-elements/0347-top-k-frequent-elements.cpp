4a52f6f3119831b6a663c55bbf8a3b3e289165f3
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-10-17 11:33:32 +0900

    Time: 17 ms (15.48%), Space: 18.2 MB (16.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,11 +4,13 @@ public:
         map<int, int> m;
         vector<vector<int>> v;
         vector<int> ans;
+
         for (int n : nums)
             m[n]++;
 
         for (auto iter = m.begin(); iter != m.end(); iter++)
             v.push_back({iter->second, iter->first});
+
         sort(v.rbegin(), v.rend());
 
         for (int i = 0; i < k; i++)
