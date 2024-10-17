f093c06bef82eff6aa2ec8726807b809143501d9
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-10-17 11:24:07 +0900

    Time: 17 ms (15.48%), Space: 18.2 MB (16.61%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> m;
+        vector<vector<int>> v;
+        vector<int> ans;
+        for (int n : nums)
+            m[n]++;
+
+        for (auto iter = m.begin(); iter != m.end(); iter++)
+            v.push_back({iter->second, iter->first});
+        sort(v.rbegin(), v.rend());
+
+        for (int i = 0; i < k; i++)
+            ans.push_back(v[i][1]);
+
+        return ans;
+    }
+};
