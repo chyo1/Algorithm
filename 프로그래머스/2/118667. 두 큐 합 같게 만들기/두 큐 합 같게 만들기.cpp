f9859d4c96f61690656df8f9be44d0edf011a89a
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-11-06 18:00:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 7.23 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -1,41 +1,45 @@
 #include <string>
 #include <vector>
 #include <queue>
-
+#include <stdio.h>
 using namespace std;
 typedef long long ll;
 
 int solution(vector<int> queue1, vector<int> queue2) {
-    int answer = 0, size = queue1.size();
+    int answer = 0, size = queue1.size(), totalSize = size * 2;
     ll sum1 = 0, sum2 = 0;
 
-    queue<int> q1, q2;
+    vector<int> v;
+    int first = 0, last = size;
     
+    // 각 배열의 합 구한 후, 주어진 배열을 벡터로 이동
     for (int i = 0; i < size; i++) {
-        sum1 += queue1[i];  q1.push(queue1[i]);
-        sum2 += queue2[i];  q2.push(queue2[i]);
+        sum1 += queue1[i];  v.push_back(queue1[i]);
+    }
+    for (int i = 0; i < size; i++) {
+        sum2 += queue2[i];  v.push_back(queue2[i]);
     }
     
+    // 모든 수의 합이 홀수
     if ((sum1 + sum2) % 2)
         return -1;
     
     long long maxCnt = 4 * size;
     while (maxCnt--) {
+        
+        // 두 큐의 합이 같다면 return
         if (sum1 == sum2)
             return answer;
-        
-        if (q1.empty() || q2.empty())
-            break;
     
         if (sum1 < sum2) {
-            int tmp = q2.front();
-            q2.pop();   q1.push(tmp);
+            int tmp = v[last];
             sum1 += tmp; sum2 -= tmp;
+            last = (last + 1) % totalSize;
         }
         else {
-            int tmp = q1.front();
-            q1.pop(); q2.push(tmp);
-            sum1 -= tmp; sum2+= tmp;
+            int tmp = v[first];
+            sum1 -= tmp; sum2 += tmp;
+            first = (first + 1) % totalSize;
         }
         answer++;
     }
