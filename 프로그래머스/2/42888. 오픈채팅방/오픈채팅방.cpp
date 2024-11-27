afdef73362f08bbb0e04142ce978ede6389baf3b
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-11-28 00:56:56 +0900

    [level 2] Title: 오픈채팅방, Time: 191.57 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -1,12 +1,9 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <iostream>
 
 using namespace std;
-
-map<string, string> info;
-vector<pair<string, string>> print;
-
 vector<string> parse(string str) {
     vector<string> strs;
     
@@ -14,41 +11,25 @@ vector<string> parse(string str) {
     for (char c : str) {
         if (c == ' ') {
             strs.push_back(str.substr(idx, len));
-            idx += len + 1;
+            cout << str.substr(idx, len) << endl;
+            idx += len;
             len = 0;
         }
         else
             len++;
+        // cout << c << endl;
     }
     strs.push_back(str.substr(idx, len));
     return strs;
 }
 
-void updateInfo(vector<string> strs) {
-    string command = strs[0], uid = strs[1];
-    
-    if (command == "Enter") {
-        print.push_back({uid, "님이 들어왔습니다."});
-        info[uid] = strs[2];
-    }
-    
-    else if (command == "Leave")
-        print.push_back({uid, "님이 나갔습니다."});
-    
-    else if (command == "Change")
-        info[uid] = strs[2];
-}
-
 vector<string> solution(vector<string> record) {
     vector<string> answer;
+    map<string, string> info;
     
     for (string r : record) {
-        vector<string> strs = parse(r);   
-        updateInfo(strs);
+        parse(r);    
     }
-    
-    for (auto p : print)
-        answer.push_back(info[p.first] + p.second);
     
     return answer;
 }
