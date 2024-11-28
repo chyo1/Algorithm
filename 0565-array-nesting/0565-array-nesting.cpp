ddf649f17629035574111c3f961fc2bc7bc0e593
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-11-28 21:30:52 +0900

    Time: 33 ms (25.96%), Space: 97.1 MB (51.76%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.cpp
+++ 0565-array-nesting/0565-array-nesting.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     bool visited[1'000'000];
-    int depth[1'000'000];
     int dfs(vector<int>& nums, int now, int nowDepth) {
         visited[now] = true;
 
         if (visited[nums[now]]) return nowDepth;
-        
-        depth[now] = dfs(nums, nums[now], nowDepth + 1);
-        return depth[now];
+
+        return dfs(nums, nums[now], nowDepth + 1);
     }
 
     int arrayNesting(vector<int>& nums) {
         int maxLen = 1;
-        for (int num : nums) {
-            if (!visited[num])
-                maxLen = max(maxLen, dfs(nums, num, 1));
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            if (!visited[num]) {
+                if (num == i)
+                    visited[i] = true;
+                else
+                    maxLen = max(maxLen, dfs(nums, num, 1));
+            }
         }
         return maxLen;
     }
