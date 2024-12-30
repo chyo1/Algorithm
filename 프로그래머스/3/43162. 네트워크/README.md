78e7f40e8c32bef5b2c92e68e30d33d385384b0f
chyo1 <142678241+chyo1@users.noreply.github.com>
2024-12-30 16:04:51 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=cpp) 
 
 ### 성능 요약
 
-메모리: 76.1 MB, 시간: 0.29 ms
+메모리: 4.13 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 30일 15:54:26
+2024년 12월 30일 16:04:43
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool visited[200] = {false, };
+void dfs(vector<vector<int>>& computers, int n, int now) {
+    visited[now] = true;
+    for (int i = 0; i < n; i++) {
+        if (computers[now][i] && !visited[i])
+            dfs(computers, n, i);
+    }
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (visited[i]) continue;
+        dfs(computers, n, i);
+        answer++;
+    }
+    return answer;
+}
