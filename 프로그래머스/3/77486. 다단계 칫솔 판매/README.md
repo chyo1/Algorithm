2b2f4ccde4be4a934348d516f9946b6f2b1316c6
chyo1 <142678241+chyo1@users.noreply.github.com>
2025-01-13 20:27:25 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 78.29 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=java) 
 
 ### 성능 요약
 
-메모리: 18.7 MB, 시간: 241.52 ms
+메모리: 126 MB, 시간: 78.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 13일 20:23:55
+2025년 01월 13일 20:27:23
 
 ### 문제 설명
 
--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -3,32 +3,27 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         HashMap <String, Integer> sequences = new HashMap<>();
-        int n = enroll.length;
-        int[] income = new int[n];
+        int[] income = new int[enroll.length];
         
         // 판매책의 조직 구성원 이름 - 순서쌍 저장
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < enroll.length; i++) 
             sequences.put(enroll[i], i);
         
         for (int i = 0; i < seller.length; i++) {
             int nowIncome = amount[i] * 100;
             String nowSeller = seller[i];
-            
+
+            income[sequences.get(nowSeller)] += nowIncome;            
             while (!nowSeller.equals("-") && nowIncome > 0) {
                 String nowGetter = referral[sequences.get(nowSeller)];
                 
-                int divide = nowIncome / 10;
+                nowIncome /= 10;
                 if (!"-".equals(nowGetter))
-                    income[sequences.get(nowGetter)] += divide;
-                
-                if (!nowSeller.equals(seller[i]))
-                    income[sequences.get(nowSeller)] -= divide;
-                
-                // System.out.println(nowSeller + " " + nowGetter + " " + divide + " " + nowIncome);
-                nowIncome = divide;
+                    income[sequences.get(nowGetter)] += nowIncome;                
+                income[sequences.get(nowSeller)] -= nowIncome;
+
                 nowSeller = nowGetter;
             }
-            income[sequences.get(seller[i])] += amount[i] * 90;
         }
         
         return income;
