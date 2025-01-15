164df13ee13ec2fd8931e690a6a3a83e9d1cc6f8
chyo1 <142678241+chyo1@users.noreply.github.com>
2025-01-15 12:35:59 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 13.85 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int[] answer = {0, 0};
+    public int[] solution(int[][] arr) {
+        int n = arr.length;
+        
+        compress(arr, 0, 0, n);
+        return answer;
+    }
+    
+    private void compress(int[][] arr, int sr, int sc, int n) {
+        int value = arr[sr][sc];
+        for (int i = sr; i < sr + n; i++) {
+            for (int j = sc; j < sc + n; j++) {
+                if (arr[i][j] != value) {
+                    n /= 2;
+                    compress(arr, sr, sc, n);
+                    compress(arr, sr, sc + n, n);
+                    compress(arr, sr + n, sc, n);
+                    compress(arr, sr + n, sc + n, n);
+                    return;
+                }
+            }
+        }
+        answer[value]++;
+        return;
+    }
+}
