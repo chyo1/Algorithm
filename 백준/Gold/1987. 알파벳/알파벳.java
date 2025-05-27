172a6650a3036fd688c7231e9e54a3795f685573
chyo1 <142678241+chyo1@users.noreply.github.com>
2025-05-27 15:40:51 +0900

    [Gold IV] Title: 알파벳, Time: 1072 ms, Memory: 15464 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -8,7 +8,6 @@ public class Main {
     private static int answer = 0;
     private static char[][] map;
     private static boolean[][] visited;
-    private static boolean[] alpha = new boolean[26];
     private static int[][] mv = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public static void main(String[] args) throws IOException {
@@ -27,27 +26,25 @@ public class Main {
             }
         }
 
-        alpha[map[0][0] - 'A'] = true;
+        int alpha = 1 << 27;
+        alpha |= 1 << (map[0][0] - 'A');
         visited[0][0] = true;
-        findMax(0, 0, 1);
+        findMax(0, 0, alpha, 1);
         System.out.println(answer);
     }
 
-    private static void findMax(int r, int c, int moveCount) {
+    private static void findMax(int r, int c, int alpha, int moveCount) {
         boolean isMoved = false;
         for (int i = 0; i < 4; i++) {
             int nr = r + mv[i][0];
             int nc = c + mv[i][1];
 
             if (nr < 0 || nc < 0 || R <= nr || C <= nc
-                    || visited[nr][nc] || alpha[map[nr][nc] - 'A']) continue;
+                    || visited[nr][nc] || (alpha & (1 << map[nr][nc] - 'A')) != 0) continue;
 
             visited[nr][nc] = true;
-            alpha[map[nr][nc] - 'A'] = true;
-            findMax(nr, nc, moveCount + 1);
+            findMax(nr, nc, alpha | 1 << (map[nr][nc] - 'A'), moveCount + 1);
             visited[nr][nc] = false;
-            alpha[map[nr][nc] - 'A'] = false;
-
             isMoved = true;
         }
 
