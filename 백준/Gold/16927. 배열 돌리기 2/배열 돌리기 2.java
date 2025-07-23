6e12494b246b74f7bb00bfe8538fe1d6c6ce5aca
chyo1 <142678241+chyo1@users.noreply.github.com>
2025-07-23 10:33:49 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 1028 ms, Memory: 42056 KB -BaekjoonHub

---
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+class Main
+{
+	static int[][] map;
+	static int N, M, R;
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = bf.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
+        R = Integer.parseInt(inputs[2]);
+         
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+        	inputs = bf.readLine().split(" ");
+        	for (int j = 0; j < M; j++) {
+        		map[i][j] = Integer.parseInt(inputs[j]);
+        	}
+        }
+        
+        int[][] mv = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        int rl = 0;
+        int rr = N - 1;
+        int cl = 0;
+        int cr = M - 1;
+        
+        while (rl < rr && cl < cr) {
+        	int r = rr;
+            int c = cr;
+            boolean flag = false;
+            List<Integer> list = new ArrayList<>();
+            int idx = 0;
+            
+	    	while (idx < 4) {
+	    		if (!(rl <= r && r <= rr && cl <= c && c <= cr) || (flag && r == rr && c == cr)) {
+	        		r -= mv[idx][0];
+	        		c -= mv[idx][1];
+	        		idx++;
+	        		if (idx == 4) break;
+	    		}
+	    		else
+	    			list.add(map[r][c]);
+	    		      		
+	    		r += mv[idx][0];
+	    		c += mv[idx][1];
+	    		flag = true;
+	    	}
+	    	
+	    	int len = list.size();
+	    	r = rr;
+	    	c = cr;
+	    	idx = 0;
+	    	flag = false;
+	    	int cnt = 0;
+    		while (idx < 4) {
+        		if (!(rl <= r && r <= rr && cl <= c && c <= cr) || (flag && r == rr && c == cr)) {
+            		r -= mv[idx][0];
+            		c -= mv[idx][1];
+            		idx++;
+            		if (idx == 4) break;
+        		}
+        		
+        		else {
+        			int now = cnt++ - R;
+        			now = now + (Math.abs(now) / len + 1) * len;
+        			map[r][c] = list.get(now % len);
+        		}
+        		      		
+        		r += mv[idx][0];
+        		c += mv[idx][1];
+        		flag = true;
+        	}
+	    	rl++; rr--;
+	    	cl++; cr--;
+        }
+      
+        print();
+
+    }
+    private static void print() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
